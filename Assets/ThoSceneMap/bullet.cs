832183c5df14a292a03919087902594c401ba4e6
Thomastert <thomas@thoemda.com>
2015-11-11 14:29:31 +0100

    walls tower etc
    :warning: :toilet: :recycle:

--- Assets/ThoSceneMap/bullet.cs
+++ Assets/ThoSceneMap/bullet.cs
@@ -1,29 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class bullet : MonoBehaviour {
+public class bullet : MonoBehaviour
+{
 
     Transform target;
     public float speed = 3f;
-
 
     void Start()
     {
         target = GameObject.FindWithTag("Player").transform;
         transform.LookAt(target.position);
-        transform.Rotate(new Vector3(0, -90, 0), Space.Self);
+        transform.Rotate(new Vector3(0, -90,0), Space.Self);
     }
 
     void Update()
     {
-       
-
-
         if (Vector3.Distance(transform.position, target.position) > 1f)
         {
-            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0,0));
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Wall")
+        {
+            Destroy(gameObject);
+        }
+
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
         }
 
     }
-
 }
